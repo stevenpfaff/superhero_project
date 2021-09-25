d4fd4cf60041319b18c02b8c9381929c17eb7d37
stevenpfaff <pfaffy1426@gmail.com>
2021-09-25 11:24:59 -0500

    through tutorial videos

--- superhero_project/superheroes/urls.py
+++ superhero_project/superheroes/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-
 from . import views
 
 app_name = 'superheroes'
 urlpatterns = [
     path('', views.index, name='index'),
-    path("<int:hero_id>/", views.detail, name='detail'),
+    path('<int:hero_id>/', views.detail, name='detail'),
     path('new/', views.create, name='create')
 ]
--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,7 +1,8 @@
+from .models import Superhero
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
-from .models import Superhero
+
 
 # Create your views here.
 def index(request):
@@ -16,16 +17,15 @@ def detail(request, hero_id):
     context = {
         'single_hero': single_hero
     }
-    return render(request, 'superheroes/detail.html', context)
+    return render(request, 'superheroes/detail.html/', context)
 
 def create(request):
     if request.method == "POST":
-        #save form contents in new object
-        name = request.POST.get("Name")
-        alter_ego = request.POST.get("Alter Ego")
-        primary = request.POST.get("Primary")
-        secondary = request.POST.get("Secondary")
-        catchphrase = request.POST.get("Catchphrase")
+        name = request.POST.get("name")
+        alter_ego = request.POST.get("alter_ego")
+        primary = request.POST.get("primary")
+        secondary = request.POST.get("secondary")
+        catchphrase = request.POST.get("catchphrase")
         new_hero = Superhero(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
         new_hero.save()
         return HttpResponseRedirect(reverse('superheroes:index'))
